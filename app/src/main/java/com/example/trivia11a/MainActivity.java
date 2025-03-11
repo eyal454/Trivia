63390e536d302182a62bad9092ad478c763b1949
eyal454 <eyalbendavid2008@gmail.com>
2025-03-11 17:28:00 +0200

    First version

--- app/src/main/java/com/example/trivia11a/MainActivity.java
+++ app/src/main/java/com/example/trivia11a/MainActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
@@ -29,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ConstraintLayout ll;
 
+    private String themeI;
+
+    private Button btnStartGame, btnSettings, btnInstructions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
         fbModule = new FbModule(this);
 
         ll = findViewById(R.id.main);
+
+        btnStartGame = findViewById(R.id.btnStart );
+        btnSettings = findViewById(R.id.onClickSetting);
+        btnInstructions = findViewById(R.id.btnInstruction);
 
         resultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -67,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickSetting(View view) {
         Intent i = new Intent(this, SettingActivity.class);
+        i.putExtra("Theme", themeI);
         resultLauncher.launch(i);
     }
 
@@ -76,42 +91,69 @@ public class MainActivity extends AppCompatActivity {
     public void setNewColorFromFb(String str) {
         //הפיירבייס קורא לפעולה בפעם הראשונה
         //ואחרי כל פעם שהמשתמש משנהאת הצבע
-        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
-        setBackgroundColor(str);
+        //Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+        setTheme(str);
 
     }
 
-    public void setBackgroundColor(String color)
+    public void setTheme(String theme)
     {
-        switch (color)
+        themeI = theme;
+
+        switch (theme)
         {
-            case "Red":
+            case "Dark":
             {
-                ll.setBackgroundColor(Color.argb(255,226, 62, 87));
+                ll.setBackgroundColor(Color.argb(255,59,59,59 ));
+                btnInstructions.setBackgroundColor(Color.argb(255,74,74,74));
+                btnSettings.setBackgroundColor(Color.argb(255,74,74,74));
+                btnStartGame.setBackgroundColor(Color.argb(255,74,74,74));
+                btnStartGame.setTextColor(Color.WHITE);
+                btnSettings.setTextColor(Color.WHITE);
+                btnInstructions.setTextColor(Color.WHITE);
+
                 break;
             }
 
-            case "Blue":
+            case "Light":
             {
-                ll.setBackgroundColor(Color.argb(255,63, 114, 175));
+                ll.setBackgroundColor(Color.argb(255,183,201,232  ));
+                btnInstructions.setBackgroundColor(Color.argb(255,224,228,242  ));
+                btnSettings.setBackgroundColor(Color.argb(255,224,228,242  ));
+                btnStartGame.setBackgroundColor(Color.argb(255,224,228,242 ));
+                btnStartGame.setTextColor(Color.BLACK);
+                btnSettings.setTextColor(Color.BLACK);
+                btnInstructions.setTextColor(Color.BLACK);
                 break;
             }
 
-            case "Pink":
+            case "Warm":
             {
-                ll.setBackgroundColor(Color.argb(255,255,192,203));
+                ll.setBackgroundColor(Color.argb(255,255,94,58 ));
+                btnInstructions.setBackgroundColor(Color.argb(255,255,127,80 ));
+                btnSettings.setBackgroundColor(Color.argb(255,255,127,80 ));
+                btnStartGame.setBackgroundColor(Color.argb(255,255,127,80 ));
+                btnStartGame.setTextColor(Color.WHITE);
+                btnSettings.setTextColor(Color.WHITE);
+                btnInstructions.setTextColor(Color.WHITE);
                 break;
             }
 
-            case "Yellow":
+            case "Cold":
             {
-                ll.setBackgroundColor(Color.argb(255,252, 227, 138));
+                ll.setBackgroundColor(Color.argb(255,128,203,196 ));
+                btnInstructions.setBackgroundColor(Color.argb(255,178,223,219 ));
+                btnSettings.setBackgroundColor(Color.argb(255,178,223,219 ));
+                btnStartGame.setBackgroundColor(Color.argb(255,178,223,219 ));
+                btnStartGame.setTextColor(Color.BLACK);
+                btnSettings.setTextColor(Color.BLACK);
+                btnInstructions.setTextColor(Color.BLACK);
                 break;
             }
 
             default:
             {
-                ll.setBackgroundColor(Color.argb(255,242, 239, 231));
+                Toast.makeText(this, "Invalid theme", Toast.LENGTH_SHORT).show();
                 break;
             }
         }
